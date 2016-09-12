bd03c341048bef8802eeb2c9953c3daa4d33aada
Charles Stafford <cstaff@Charless-MacBook-Pro.local>
2016-09-12 11:11:58 -0600

    express setup

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const expressJWT = require('express-jwt');
+const logger = require('morgan');
+
+
+//Start app
+const app = express();
+
+//port setup
+const port = process.env.PORT || 3000;
+
+app.use(logger);
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+  extended: false
+}));
+app.use(cors);
+
+
+
+
+
+
+//Setup Error Handler
+app.use(function(req, res, next){
+  let err = new Error('Route not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+
+//listen on port
+app.listen(port, function(){
+  console.log("Listening on: ", port);
+});
+
+//export
+module.export = app;
