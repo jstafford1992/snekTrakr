1014ae4fce1ca6e36ce71a3da1450d135eb657e4
Charles Stafford <cstaff@Charless-MacBook-Pro.local>
2016-09-20 13:35:37 -0600

    updated the way data is recieved on the front end

--- routes/snakes.js
+++ routes/snakes.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next){
   .where('user_id', req.user.id)
   .select('*')
   .then(function(data){
-    res.send(data);
+    res.json(data);
   })
   .catch(function(err){
     throw new Error();
@@ -22,47 +22,132 @@ router.get('/', function(req, res, next){
 
 router.get('/:id', function(req, res, next){
   //TODO will list individual snake for detailed report
+  // var gender;
+  // knex('snakes')
+  // .select('sex')
+  // .where('id', req.params.id)
+  // .then(function(data){
+  //      gender = data[0].sex;
+  //      if (gender === "female") {
+  //        knex('snakes')
+  //        .where('snakes.id', req.params.id)
+  //        .join('users', 'snakes.user_id', '=', 'users.id' )
+  //        .join('shed', 'snakes.id', '=', 'shed.snake_id')
+  //        .join('weight', 'snakes.id', '=', 'weight.snake_id')
+  //        .join('feeding', 'snakes.id', '=', 'feeding.snake_id')
+  //        .join('breeding', 'snakes.id', '=', 'breeding.snake_id')
+  //        .join('clutch', 'snakes.id', '=', 'clutch.snake_id')
+  //        .select('snakes.id', 'snakes.name', 'snakes.notes', 'snakes.sex', 'snakes.year_hatched', 'snakes.group', 'snakes.url', 'snakes.image_url', 'shed.date_shed', 'shed.prelay',  'weight.weight', 'weight.date_weighed', 'feeding.rat_size', 'feeding.successful', 'feeding.attempted', 'breeding.sire', 'breeding.date_paired', 'clutch.notes', 'clutch.date_layed', 'clutch.number_layed', 'clutch.bad_eggs')
+  //        .then(function(data){
+  //          res.json(data);
+  //        })
+  //        .catch(function(err){
+  //          throw new Error(err);
+  //        });
+  //      } else {
+  //        knex('snakes')
+  //        .where('snakes.id', req.params.id)
+  //       //  .join('users', 'snakes.user_id', '=', 'users.id' )
+  //        .join('shed', 'snakes.id', '=', 'shed.snake_id')
+  //        .join('weight', 'snakes.id', '=', 'weight.snake_id')
+  //        .join('feeding', 'snakes.id', '=', 'feeding.snake_id')
+  //        .join('breeding', 'snakes.id', '=', 'breeding.sire')
+  //        .select('*')
+  //        .then(function(data){
+  //          res.json(data);
+  //        })
+  //        .catch(function(err){
+  //          throw new Error(err);
+  //        });
+  //      }
+  // }).catch(function(err){
+  //   console.error(err);
+  // });
   var gender;
-  knex('snakes')
-  .select('sex')
-  .where('id', req.params.id)
-  .then(function(data){
-       gender = data[0].sex;
-       if (gender === "female") {
-         knex('snakes')
-         .where('snakes.id', req.params.id)
-         .join('users', 'snakes.user_id', '=', 'users.id' )
-         .join('shed', 'snakes.id', '=', 'shed.snake_id')
-         .join('weight', 'snakes.id', '=', 'weight.snake_id')
-         .join('feeding', 'snakes.id', '=', 'feeding.snake_id')
-         .join('breeding', 'snakes.id', '=', 'breeding.snake_id')
-         .join('clutch', 'snakes.id', '=', 'clutch.snake_id')
-         .select('snakes.id', 'snakes.name', 'snakes.notes', 'snakes.sex', 'snakes.year_hatched', 'snakes.group', 'snakes.url', 'snakes.image_url', 'shed.date_shed', 'shed.prelay',  'weight.weight', 'weight.date_weighed', 'feeding.rat_size', 'feeding.successful', 'feeding.attempted', 'breeding.sire', 'breeding.date_paired', 'clutch.notes', 'clutch.date_layed', 'clutch.number_layed', 'clutch.bad_eggs')
-         .then(function(data){
-           res.json(data[0]);
-         })
-         .catch(function(err){
-           throw new Error(err);
-         });
-       } else {
-         knex('snakes')
-         .where('snakes.id', req.params.id)
-        //  .join('users', 'snakes.user_id', '=', 'users.id' )
-         .join('shed', 'snakes.id', '=', 'shed.snake_id')
-         .join('weight', 'snakes.id', '=', 'weight.snake_id')
-         .join('feeding', 'snakes.id', '=', 'feeding.snake_id')
-         .join('breeding', 'snakes.id', '=', 'breeding.sire')
-         .select('*')
-         .then(function(data){
-           res.json(data);
-         })
-         .catch(function(err){
-           throw new Error(err);
-         });
-       }
+  var snake = {};
+  var shed = {};
+  var weight = {};
+  var feed = {};
+  var pair = {};
+
+  var container = {};
+
+  knex('snakes').select('*').where('id', req.params.id).then(function(data){
+    console.log("Snakes: ", data);
+    gender = data[0].sex;
+    snake.data = data;
+    if (gender == "male") {
+      console.log(gender);
+      var breedingMale = knex('breeding')
+      .select('*')
+      .where('sire', req.params.id)
+      .then(function(data){
+        // console.log("Breeding Male");
+        container.pair = data;
+      });
+      Promise.all([shedding, weighing, feeding, breedingMale]);
+      return setTimeout(function(){
+          console.log("Inside Timeout: ", container);
+        return res.json(container);
+      }, 50);
+    } else {
+      var breedingFemale = knex('breeding')
+      .select('*')
+      .where('snake_id', req.params.id)
+      .then(function(data){
+        // console.log("Pair: ", data);
+        // res.json(data);
+        // console.log("Breeding Female");
+        container.pair = data;
+      });
+      Promise.all([shedding, weighing, feeding, breedingFemale]);
+      return setTimeout(function(){
+          console.log("Inside Timeout: ", container);
+        return res.json(container);
+      }, 50);
+    }
+
+    // res.json(container)
+
   }).catch(function(err){
-    console.error(err);
+    console.log(err);
+    res.status(500).json({
+      err:err
+    });
   });
+
+  var shedding = knex('shed')
+  .select('*')
+  .where('snake_id', req.params.id)
+  .then(function(data){
+    // console.log("Shed: ", data);
+    container.shed = data;
+    // res.json(data);
+  });
+
+  var weighing = knex('weight')
+  .select('*')
+  .where('snake_id', req.params.id)
+  .then(function(data){
+    // console.log("Weight: ", data);
+    // res.json(data);
+    container.weight = data;
+  });
+
+  var feeding = knex('feeding')
+  .select('*')
+  .where('snake_id', req.params.id)
+  .then(function(data){
+    // console.log("Feeding: ", data);
+    // res.json(data);
+    container.feed = data;
+  });
+
+
+
+
+
+
 
 });
 
@@ -85,11 +170,11 @@ router.post('/new', function(req, res, next){
   .insert(snake)
   .then(function(data){
     console.log(data);
-    return res.json("success");
+    res.json("success");
   })
   .catch(function(err){
     console.log(err);
-    return "failed insert";
+     res.json("failed insert");
   });
 
 
@@ -115,7 +200,7 @@ router.put('/:id', function(req, res, next){
   .update(snake, '*')
   .then(function(data){
     console.log(data);
-    res.json('Success');
+    res.json(data);
   })
   .catch(function(err){
     console.log(err);
